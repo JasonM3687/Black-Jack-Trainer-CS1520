cbe451da957bdb300d93e706b4feb451fbbd80cb
Bilal Riaz <bilalriaz@Bilals-MacBook-Pro.local>
2021-09-29 22:27:57 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import react from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,19 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Welcome to BlackJack Trainer</h1>
+        <img src="https://i.ibb.co/6FJ0JGK/561-5611765-all-diamonds-cards-clip-arts-cards-fanned-out.png" className="App-logo" alt="logo" height="200" width="300" />
+        <p>Welcome to an online web application that can be utilized to train and help memorize basic blackjack strategy.</p>
+        <ul>
+          <li>You will receive strategy recommendations based on the basic strategy chart</li>
+          <li>You will significantly hone your blackjack skills, preparing you for the real table!</li>
+        </ul>
+        <img src="https://www.blackjackapprenticeship.com/wp-content/uploads/2018/10/mini-blackjack-strategy-chart.png" with="1000"/>
+        </header>
     </div>
   );
 }
