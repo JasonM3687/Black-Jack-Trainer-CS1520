84b40e69e1e53f473765834fb180907d24c1b1a3
Bilal Riaz <bilalriaz@Bilals-MacBook-Pro.local>
2021-12-06 00:01:50 -0500

    Added Trainer Functionality

--- src/BlackJack.tsx
+++ src/BlackJack.tsx
@@ -264,6 +264,709 @@ const BlackJack = () => {
     }
   }
 
+  const ProblemOnClick = () => {
+    window.location.href = "https://www.1800gambler.net/";
+  }
+  
+  const chippySaysHit = () => {
+    // Dealer has 2
+    if (userScore == 2 && dealerScore == 2){
+        return true
+    }
+    else if (userScore == 3 && dealerScore == 2){
+        return true
+    }
+    else if(userScore == 4 && dealerScore == 2) {
+        return true
+    }
+    else if(userScore == 5 && dealerScore == 2) {
+        return true
+    }
+    else if(userScore == 6 && dealerScore == 2) {
+      return true
+    }
+    else if(userScore == 7 && dealerScore == 2) {
+      return true
+    }
+    else if(userScore == 8 && dealerScore == 2) {
+      return true
+    }
+    else if(userScore == 9 && dealerScore == 2) {
+      return true
+    }
+    else if(userScore == 10 && dealerScore == 2) {
+      return true
+    }
+    else if(userScore == 11 && dealerScore == 2) {
+      return true
+    }
+    else if(userScore == 12 && dealerScore == 2) {
+      return true
+    }
+    // Dealer has 3
+    else if (userScore == 2 && dealerScore == 3){
+      return true
+    }
+    else if(userScore == 3 && dealerScore == 3) {
+      return true
+    }
+    else if(userScore == 4 && dealerScore == 3) {
+      return true
+    }
+    else if(userScore == 5 && dealerScore == 3) {
+      return true
+    }
+    else if(userScore == 6 && dealerScore == 3) {
+      return true
+    }
+    else if(userScore == 7 && dealerScore == 3) {
+      return true
+    }
+    else if(userScore == 8 && dealerScore == 3) {
+      return true
+    }
+    else if(userScore == 9 && dealerScore == 3) {
+      return true
+    }
+    else if(userScore == 10 && dealerScore == 3) {
+      return true
+    }
+    else if(userScore == 11 && dealerScore == 3) {
+      return true
+    }
+    else if(userScore == 12 && dealerScore == 3) {
+      return true
+    }
+    // Dealer has 4
+    else if (userScore == 2 && dealerScore == 4){
+      return true
+    }
+    else if(userScore == 3 && dealerScore == 4) {
+      return true
+    }
+    else if(userScore == 4 && dealerScore == 4) {
+      return true
+    }
+    else if(userScore == 5 && dealerScore == 4) {
+      return true
+    }
+    else if(userScore == 6 && dealerScore == 4) {
+      return true
+    }
+    else if(userScore == 7 && dealerScore == 4) {
+      return true
+    }
+    else if(userScore == 8 && dealerScore == 4) {
+      return true
+    }
+    else if(userScore == 9 && dealerScore == 4) {
+      return true
+    }
+    else if(userScore == 10 && dealerScore == 4) {
+      return true
+    }
+    else if(userScore == 11 && dealerScore == 4) {
+      return true
+    }
+    else if(userScore == 12 && dealerScore == 4) {
+      return true
+    }
+    // Dealer has 5
+    else if (userScore == 2 && dealerScore == 5){
+      return true
+    }
+    else if(userScore == 3 && dealerScore == 5) {
+      return true
+    }
+    else if(userScore == 4 && dealerScore == 5) {
+      return true
+    }
+    else if(userScore == 5 && dealerScore == 5) {
+      return true
+    }
+    else if(userScore == 6 && dealerScore == 5) {
+      return true
+    }
+    else if(userScore == 7 && dealerScore == 5) {
+      return true
+    }
+    else if(userScore == 8 && dealerScore == 5) {
+      return true
+    }
+    else if(userScore == 9 && dealerScore == 5) {
+      return true
+    }
+    else if(userScore == 10 && dealerScore == 5) {
+      return true
+    }
+    else if(userScore == 11 && dealerScore == 5) {
+      return true
+    }
+    else if(userScore == 12 && dealerScore == 5) {
+      return true
+    }
+    // Dealer has 6
+    else if (userScore == 2 && dealerScore == 6){
+      return true
+    }
+    else if(userScore == 3 && dealerScore == 6) {
+      return true
+    }
+    else if(userScore == 4 && dealerScore == 6) {
+      return true
+    }
+    else if(userScore == 5 && dealerScore == 6) {
+      return true
+    }
+    else if(userScore == 6 && dealerScore == 6) {
+      return true
+    }
+    else if(userScore == 7 && dealerScore == 6) {
+      return true
+    }
+    else if(userScore == 8 && dealerScore == 6) {
+      return true
+    }
+    else if(userScore == 9 && dealerScore == 6) {
+      return true
+    }
+    else if(userScore == 10 && dealerScore == 6) {
+      return true
+    }
+    else if(userScore == 11 && dealerScore == 6) {
+      return true
+    }
+    else if(userScore == 12 && dealerScore == 6) {
+      return true
+    }
+    //Dealer has 7
+    else if (userScore == 2 && dealerScore == 7){
+      return true
+    }
+    else if(userScore == 3 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 4 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 5 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 6 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 7 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 8 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 9 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 10 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 11 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 12 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 13 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 14 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 15 && dealerScore == 7) {
+      return true
+    }
+    else if(userScore == 16 && dealerScore == 7) {
+      return true
+    }
+    // Dealer has 8
+    else if (userScore == 2 && dealerScore == 8){
+      return true
+    }
+    else if(userScore == 3 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 4 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 5 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 6 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 7 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 8 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 9 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 10 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 11 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 12 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 13 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 14 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 15 && dealerScore == 8) {
+      return true
+    }
+    else if(userScore == 16 && dealerScore == 8) {
+      return true
+    }
+    // Dealer has 9
+    else if (userScore == 2 && dealerScore == 9){
+      return true
+    }
+    else if(userScore == 3 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 4 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 5 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 6 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 7 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 8 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 9 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 10 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 11 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 12 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 13 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 14 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 15 && dealerScore == 9) {
+      return true
+    }
+    else if(userScore == 16 && dealerScore == 9) {
+      return true
+    }
+    // Dealer has 10
+    else if (userScore == 2 && dealerScore == 10){
+      return true
+    }
+    else if(userScore == 3 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 4 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 5 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 6 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 7 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 8 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 9 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 10 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 11 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 12 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 13 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 14 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 15 && dealerScore == 10) {
+      return true
+    }
+    else if(userScore == 16 && dealerScore == 10) {
+      return true
+    }
+    // Dealer has 11 (Ace)
+    else if (userScore == 2 && dealerScore == 11){
+      return true
+    }
+    else if(userScore == 3 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 4 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 5 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 6 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 7 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 8 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 9 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 10 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 11 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 12 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 13 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 14 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 15 && dealerScore == 11) {
+      return true
+    }
+    else if(userScore == 16 && dealerScore == 11) {
+      return true
+    }
+    else
+      return false
+  }
+  
+  const chippySaysStand = () => {
+    // Dealer has 2
+    if (userScore == 13 && dealerScore == 2) {
+      return true
+    }
+    else if (userScore == 14 && dealerScore == 2) {
+      return true
+    }
+    else if (userScore == 15 && dealerScore == 2) {
+      return true
+    }
+    else if (userScore == 16 && dealerScore == 2) {
+      return true
+    }
+    else if (userScore == 17 && dealerScore == 2) {
+      return true
+    }
+    else if (userScore == 18 && dealerScore == 2) {
+      return true
+    }
+    else if (userScore == 19 && dealerScore == 2) {
+      return true
+    }
+    else if (userScore == 20 && dealerScore == 2) {
+      return true
+    }
+    else if (userScore == 21 && dealerScore == 2) {
+      return true
+    }
+    // Dealer has 3
+    else if (userScore == 13 && dealerScore == 3) {
+      return true
+    }
+    else if (userScore == 14 && dealerScore == 3) {
+      return true
+    }
+    else if (userScore == 15 && dealerScore == 3) {
+      return true
+    }
+    else if (userScore == 16 && dealerScore == 3) {
+      return true
+    }
+    else if (userScore == 17 && dealerScore == 3) {
+      return true
+    }
+    else if (userScore == 18 && dealerScore == 3) {
+      return true
+    }
+    else if (userScore == 19 && dealerScore == 3) {
+      return true
+    }
+    else if (userScore == 20 && dealerScore == 3) {
+      return true
+    }
+    else if (userScore == 21 && dealerScore == 3) {
+      return true
+    }
+    // Dealer has 4
+    else if (userScore == 12 && dealerScore == 4) {
+      return true
+    }
+    else if (userScore == 13 && dealerScore == 4) {
+      return true
+    }
+    else if (userScore == 14 && dealerScore == 4) {
+      return true
+    }
+    else if (userScore == 15 && dealerScore == 4) {
+      return true
+    }
+    else if (userScore == 16 && dealerScore == 4) {
+      return true
+    }
+    else if (userScore == 17 && dealerScore == 4) {
+      return true
+    }
+    else if (userScore == 18 && dealerScore == 4) {
+      return true
+    }
+    else if (userScore == 19 && dealerScore == 4) {
+      return true
+    }
+    else if (userScore == 20 && dealerScore == 4) {
+      return true
+    }
+    else if (userScore == 21 && dealerScore == 4) {
+      return true
+    }
+    // Dealer has 5
+    else if (userScore == 12 && dealerScore == 5) {
+      return true
+    }
+    else if (userScore == 13 && dealerScore == 5) {
+      return true
+    }
+    else if (userScore == 14 && dealerScore == 5) {
+      return true
+    }
+    else if (userScore == 15 && dealerScore == 5) {
+      return true
+    }
+    else if (userScore == 16 && dealerScore == 5) {
+      return true
+    }
+    else if (userScore == 17 && dealerScore == 5) {
+      return true
+    }
+    else if (userScore == 18 && dealerScore == 5) {
+      return true
+    }
+    else if (userScore == 19 && dealerScore == 5) {
+      return true
+    }
+    else if (userScore == 20 && dealerScore == 5) {
+      return true
+    }
+    else if (userScore == 21 && dealerScore == 5) {
+      return true
+    }
+    // Dealer has 6
+    else if (userScore == 12 && dealerScore == 6) {
+      return true
+    }
+    else if (userScore == 13 && dealerScore == 6) {
+      return true
+    }
+    else if (userScore == 14 && dealerScore == 6) {
+      return true
+    }
+    else if (userScore == 15 && dealerScore == 6) {
+      return true
+    }
+    else if (userScore == 16 && dealerScore == 6) {
+      return true
+    }
+    else if (userScore == 17 && dealerScore == 6) {
+      return true
+    }
+    else if (userScore == 18 && dealerScore == 6) {
+      return true
+    }
+    else if (userScore == 19 && dealerScore == 6) {
+      return true
+    }
+    else if (userScore == 20 && dealerScore == 6) {
+      return true
+    }
+    else if (userScore == 21 && dealerScore == 6) {
+      return true
+    }
+    // Dealer has 7
+    else if (userScore == 17 && dealerScore == 7) {
+      return true
+    }
+    else if (userScore == 18 && dealerScore == 7) {
+      return true
+    }
+    else if (userScore == 19 && dealerScore == 7) {
+      return true
+    }
+    else if (userScore == 20 && dealerScore == 7) {
+      return true
+    }
+    else if (userScore == 21 && dealerScore == 7) {
+      return true
+    }
+    // Dealer has 8
+    else if (userScore == 17 && dealerScore == 8) {
+      return true
+    }
+    else if (userScore == 18 && dealerScore == 8) {
+      return true
+    }
+    else if (userScore == 19 && dealerScore == 8) {
+      return true
+    }
+    else if (userScore == 20 && dealerScore == 8) {
+      return true
+    }
+    else if (userScore == 21 && dealerScore == 8) {
+      return true
+    }
+    // Dealer has 9
+    else if (userScore == 17 && dealerScore == 9) {
+      return true
+    }
+    else if (userScore == 18 && dealerScore == 9) {
+      return true
+    }
+    else if (userScore == 19 && dealerScore == 9) {
+      return true
+    }
+    else if (userScore == 20 && dealerScore == 9) {
+      return true
+    }
+    else if (userScore == 21 && dealerScore == 9) {
+      return true
+    }
+    // Dealer has 10
+    else if (userScore == 17 && dealerScore == 10) {
+      return true
+    }
+    else if (userScore == 18 && dealerScore == 10) {
+      return true
+    }
+    else if (userScore == 19 && dealerScore == 10) {
+      return true
+    }
+    else if (userScore == 20 && dealerScore == 10) {
+      return true
+    }
+    else if (userScore == 21 && dealerScore == 10) {
+      return true
+    }
+    // Dealer has 11 (Ace)
+    else if (userScore == 17 && dealerScore == 11) {
+      return true
+    }
+    else if (userScore == 18 && dealerScore == 11) {
+      return true
+    }
+    else if (userScore == 19 && dealerScore == 11) {
+      return true
+    }
+    else if (userScore == 20 && dealerScore == 11) {
+      return true
+    }
+    else if (userScore == 21 && dealerScore == 11) {
+      return true
+    }
+
+  }
+  if (chippySaysHit() == true)
+  {
+    return (
+      <div className="game">
+        <Button 
+            className="exitButton"
+            variant="contained" 
+            color="error" 
+            component={Link} to="/"
+            >
+            Exit Game 
+        </Button>
+        <Status message={message} balance={balance} />
+        <Controls
+          balance={balance}
+          gameState={gameState}
+          buttonState={buttonState}
+          betEvent={placeBet}
+          hitEvent={hit}
+          standEvent={stand}
+          resetEvent={resetGame}
+        />
+        <Hand title={`Dealer's Hand (${dealerScore})`} cards={dealerCards} />
+        <Hand title={`Your Hand (${userScore})`} cards={userCards} />
+        <img src="https://i.ibb.co/cbCnR6c/chippy-removebg-preview.png" className="chippyRight"/>
+        <img src="https://i.ibb.co/NT2GCD5/newHit.png" className="chippyHit1"/>
+        <img src="https://i.ibb.co/Nr4cmgW/1800.jpg" className="problem" onClick={ProblemOnClick}/>
+        <p className="trainerChippy">Trainer Chippy</p>
+      </div>
+    );
+  }
+  else if (chippySaysStand() == true)
+  {
+    return (
+      <div className="game">
+        <Button 
+            className="exitButton"
+            variant="contained" 
+            color="error" 
+            component={Link} to="/"
+            >
+            Exit Game 
+        </Button>
+        <Status message={message} balance={balance} />
+        <Controls
+          balance={balance}
+          gameState={gameState}
+          buttonState={buttonState}
+          betEvent={placeBet}
+          hitEvent={hit}
+          standEvent={stand}
+          resetEvent={resetGame}
+        />
+        <Hand title={`Dealer's Hand (${dealerScore})`} cards={dealerCards} />
+        <Hand title={`Your Hand (${userScore})`} cards={userCards} />
+        <img src="https://i.ibb.co/cbCnR6c/chippy-removebg-preview.png" className="chippyRight"/>
+        <img src="https://i.ibb.co/W0FGNKD/chippy-stand1.png" className="chippyStand1"/>
+        <img src="https://i.ibb.co/Nr4cmgW/1800.jpg" className="problem" onClick={ProblemOnClick}/>
+        <p className="trainerChippy">Trainer Chippy</p>
+      </div>
+    );
+  }
   return (
     <div className="game">
       <Button 
@@ -287,6 +990,7 @@ const BlackJack = () => {
       <Hand title={`Dealer's Hand (${dealerScore})`} cards={dealerCards} />
       <Hand title={`Your Hand (${userScore})`} cards={userCards} />
       <img src="https://i.ibb.co/cbCnR6c/chippy-removebg-preview.png" className="chippyRight"/>
+      <img src="https://i.ibb.co/Nr4cmgW/1800.jpg" className="problem" onClick={ProblemOnClick}/>
       <p className="trainerChippy">Trainer Chippy</p>
     </div>
   );
